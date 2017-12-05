39258d3ae17e887461fa374dc2225b76ac15aa5a
jakoblorz <jakob@lorz.org>
2017-12-05 13:08:57 +0100

    implemented functions to parse a simple jsx-dom-tree

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var loadDomTree = exports.loadDomTree = require("./dist/render").loadDomTree;
+var renderJSONDomTree = exports.renderJSONDomTree = require("./dist/render").renderJSONDomTree;
+var renderHTMLDomTree = exports.renderHTMLDomTree = require("./dist/render").renderHTMLDomTree;
+var renderXMLDomTree = exports.renderXMLDomTree = require("./dist/render").renderXMLDomTree;
---
+++ src/render.js
@@ -0,0 +1,29 @@
+// overall idea: credits to "https://jasonformat.com/wtf-is-jsx/"
+
+export const loadDomTree = function (nodeName, attributes, ...args) {
+    let children = args.length ? [].concat(...args) : null;
+    return { nodeName, attributes, children };
+};
+
+export const renderJSONDomTree = function (vnode) {
+    return JSON.stringify(vnode);
+};
+
+export const renderHTMLDomTree = function (vnode) {
+    // Strings just convert to #text Nodes:
+    if (vnode.split) return document.createTextNode(vnode);
+
+    // create a DOM element with the nodeName of our VDOM element:
+    let n = document.createElement(vnode.nodeName);
+
+    // copy attributes onto the new node:
+    let a = vnode.attributes || {};
+    Object.keys(a).forEach( k => n.setAttribute(k, a[k]) );
+
+    // render (build) and then append child nodes:
+    (vnode.children || []).forEach( c => n.appendChild(render(c)) );
+
+    return n;
+};
+
+export const renderXMLDomTree = renderHTMLDomTree;
