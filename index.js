557a927aa7413d2f1e8fbc0b9337f167b0722d77
jakoblorz <jakob@lorz.org>
2017-12-09 16:41:48 +0100

    simplified project

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-"use strict";
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
+const createDomRenderer = function (fn) {
+    return function (nodeName, attributes, ...args) {
+        let children = args.length ? [].concat(...args) : null;
+        return fn(nodeName, attributes, children);
+    }
+}
+
+const renderJSONDomTree = createDomRenderer(function (nodeName, attributes, children) {
+    let serializedChildString = "[]";
+    
+    const isJsonFormat = function (val) {
+        return val[0] === "{" && val[1] === '"' && val[val.length - 1] === "}";
+    };
+
+    const concatChildrenString = function (s, c) {
+        if (isJsonFormat(c)) {
+            return s + "," + c.toString();
+        }
+
+        return s + ',"' + c + '"';
+    }
+
+    if (children && children instanceof Array) {
+        serializedChildString = "[" + children.reduce(
+            concatChildrenString, "").slice(1) + "]";
+    }
+
+    const serializedContent = JSON.stringify({ nodeName, attributes });
+    return serializedContent.slice(0, -1) + ',"children":' + serializedChildString + "}";
 });
 
-var createDomRenderer = exports.createDomRenderer = require("./dist/render").createDomRenderer;
-var loadDomTree = exports.loadDomTree = require("./dist/render").loadDomTree;
-var renderJSONDomTree = exports.renderJSONDomTree = require("./dist/json").renderJSONDomTree;
-var renderJSONDomToString = exports.renderJSONDomToString = require("./dist/json").renderJSONDomToString;
-var renderHTMLDomTree = exports.renderHTMLDomTree = require("./dist/html").renderHTMLDomTree;
-var renderReactDomTree = exports.renderReactDomTree = require("./dist/react").renderReactDomTree;
-var renderReactDomToString = exports.renderReactDomToString = require("./dist/react").renderReactDomToString;
+const renderHTMLDomTree = createDomRenderer(function (nodeName, attributes, children) {
+    
+    const attribs = Object.keys(attributes ? attributes : {}).reduce((atts, att) =>
+        atts + " " + att + '="' + attributes[att] + '"', "");
+
+    if (!children) {
+        return "<" + nodeName + attribs + "/>";
+    }
+
+    const childs = children.reduce((s, c) => s + c, "");
+
+    return "<" + nodeName + attribs + ">" + childs + "</" + nodeName + ">";
+});
+
+module.exports.createRenderer = function (format) {
+    switch (format) {
+        case "json":
+            return renderJSONDomTree;
+        case "html":
+            return renderHTMLDomTree;
+        default:
+            throw new Error(format + " is not a supported format");
+            break;
+    }
+}
--- src/react.js
+++
@@ -1,6 +0,0 @@
-import { createDomRenderer } from "./render";
-import { createElement } from "react";
-import { renderToString } from "react-dom/server";
-
-export const renderReactDomTree = createElement;
-export const renderReactDomToString = renderToString;
